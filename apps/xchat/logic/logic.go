aeb9ede34263be176c65e9aa0e514a1df8c3b6de
webee.yw <webee.yw@gmail.com>
2016-05-13 15:38:56 +0800

    refactor.

--- apps/xchat/logic/logic.go
+++ apps/xchat/logic/logic.go
@@ -22,6 +22,10 @@ func Start(config *Config) {
 	}
 	log.Println("joined xchat:", d)
 
+	subreg(c)
+}
+
+func subreg(c *turnpike.Client) {
 	if err := c.Subscribe(URIWAMPSessionOnJoin, onJoin); err != nil {
 		log.Fatalf("Error subscribing to %s: %s\n", URIWAMPSessionOnJoin, err)
 	}
@@ -37,20 +41,24 @@ func Start(config *Config) {
 
 func onJoin(args []interface{}, kwargs map[string]interface{}) {
 	details := args[0].(map[string]interface{})
+	user := details["authid"].(string)
 	sessionID := uint64(details["session"].(float64))
 	role := details["authrole"].(string)
-	log.Printf("<%s:%d> joined\n", role, sessionID)
+	log.Printf("<%s:%s:%d> joined\n", role, user, sessionID)
+	// register this user.
 }
 
 func onLeave(args []interface{}, kwargs map[string]interface{}) {
 	sessionID := uint64(args[0].(float64))
 	log.Printf("<%d> left\n", sessionID)
+	// unregister this user.
 }
 
 func toUpper(args []interface{}, kargs map[string]interface{}, details map[string]interface{}) (result *turnpike.CallResult) {
+	user := details["caller_authid"].(string)
 	caller := uint64(details["caller"].(float64))
 	role := details["caller_authrole"].(string)
-	log.Printf("<%s:%d> [rpc]%s: %v, %v, %v\n", role, caller, URITestToUpper, args, kargs, details)
+	log.Printf("<%s:%s:%d> [rpc]%s: %v, %v, %v\n", role, user, caller, URITestToUpper, args, kargs, details)
 	s, ok := args[0].(string)
 	if !ok {
 		return &turnpike.CallResult{Err: turnpike.URI("xchat.invalid-argument")}
