08367fa1ffd282f457822b0017aaf2d55e8a0646
webee.yw <webee.yw@gmail.com>
2016-05-14 23:23:46 +0800

    refactor with jwtutils.

--- broker/userboard/auth.go
+++ broker/userboard/auth.go
@@ -1,16 +1,13 @@
 package userboard
 
 import (
-	"fmt"
-
 	"xim/broker/userds"
-
-	"github.com/dgrijalva/jwt-go"
+	"xim/utils/jwtutils"
 )
 
 // VerifyAppAuthToken verify app token.
 func VerifyAppAuthToken(auth string) (aid *userds.AppIdentity, err error) {
-	token, err := parseToken(auth, appKey)
+	token, err := jwtutils.ParseToken(auth, appKey)
 
 	if err != nil || !token.Valid {
 		return nil, err
@@ -24,7 +21,7 @@ func VerifyAppAuthToken(auth string) (aid *userds.AppIdentity, err error) {
 
 // VerifyAuthToken verify user token.
 func VerifyAuthToken(auth string) (uid *userds.UserIdentity, err error) {
-	token, err := parseToken(auth, userKey)
+	token, err := jwtutils.ParseToken(auth, userKey)
 	if err != nil || !token.Valid {
 		return nil, err
 	}
@@ -33,14 +30,4 @@ func VerifyAuthToken(auth string) (uid *userds.UserIdentity, err error) {
 		User: token.Claims["user"].(string),
 	}
 	return uid, nil
-}
-
-func parseToken(authToken string, key []byte) (*jwt.Token, error) {
-	return jwt.Parse(authToken, func(token *jwt.Token) (interface{}, error) {
-		// Check the signing method
-		if token.Method.Alg() != jwt.SigningMethodHS256.Name {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Method.Alg())
-		}
-		return key, nil
-	})
 }
