417a59a47c2c81d88f166bf22f6749f2f7d6fa3c
webee.yw <webee.yw@gmail.com>
2016-05-21 11:05:54 +0800

    fix database.

--- apps/xchat/db/db.go
+++ apps/xchat/db/db.go
@@ -22,19 +22,12 @@ type Chat struct {
 	ID      uint64 `db:"id"`
 	Type    string
 	Channel string
-	Title   string
-}
-
-// Member is the chat member.
-type Member struct {
-	ChatID uint64 `db:"chat_id"`
-	User   string
 }
 
 // GetChatByChannel get chat by channel name.
 func GetChatByChannel(channel string) (*Chat, error) {
 	chat := Chat{}
-	if err := db.Get(&chat, `SELECT id, type, channel, title FROM xchat_chat where channel=$1`, channel); err != nil {
+	if err := db.Get(&chat, `SELECT id, type, channel FROM xchat_chat where channel=$1`, channel); err != nil {
 		return nil, err
 	}
 	return &chat, nil
@@ -43,6 +36,6 @@ func GetChatByChannel(channel string) (*Chat, error) {
 // GetChannelByChatIDAndUser get channel by chat id and user.
 func GetChannelByChatIDAndUser(chatID uint64, user string) (string, error) {
 	var channel string
-	err := db.Get(&channel, `SELECT c.channel FROM xchat_chat c left join xchat_member m on c.id = m.chat_id where c.id=$1 and m.user=$2`, chatID, user)
+	err := db.Get(&channel, `SELECT c.channel FROM xchat_chat c left join xchat_member m on c.id = m.chat_id left join xchat_user u on u.id = m.user_id where c.id=$1 and u.user=$2`, chatID, user)
 	return channel, err
 }
