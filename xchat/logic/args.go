2bc71247ebaddf52d2a025c8a3886fc907ba35ad
webee.yw <webee.yw@gmail.com>
2016-06-15 18:18:28 +0800

    update configs.

--- xchat/logic/args.go
+++ xchat/logic/args.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"flag"
+	"io/ioutil"
+	"log"
+	"path"
 	"xim/utils/argsutils"
+	"xim/utils/envutils"
 )
 
 // Args is app's arguments.
@@ -25,7 +29,16 @@ var (
 	}
 )
 
+func readFile(path string) string {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalln("read file err:", err)
+	}
+	return string(data)
+}
+
 func init() {
+	env := envutils.GetEnvDefault("XCHAT_ENV", "dev")
 	flag.BoolVar(&args.dial, "dial", false, "rpc service dial to addr(rep/req proxy)")
 	flag.Var(args.addrs, "addr", "rpc listen/dial addresses.")
 	flag.Var(args.pubAddrs, "pub-addr", "publisher listen/dial addresses.")
@@ -33,6 +46,6 @@ func init() {
 	flag.BoolVar(&args.debug, "debug", false, "whether to enable debug tools.")
 	flag.StringVar(&args.pprofAddr, "pprof-addr", "localhost:6061", "debug pprof http address.")
 	flag.StringVar(&args.dbDriverName, "db-driver-name", "postgres", "database driver name.")
-	flag.StringVar(&args.dbDatasourceName, "db-datasource-name", "postgres://xchat:xchat1234@localhost:5432/xchat?sslmode=disable", "database datasoure name.")
+	flag.StringVar(&args.dbDatasourceName, "db-datasource-name", readFile(path.Join("conf", env, "xchat/dbconn.txt")), "database datasoure name.")
 	flag.IntVar(&args.dbMaxConn, "db-max-conn", 200, "database connection pool max connections.")
 }
