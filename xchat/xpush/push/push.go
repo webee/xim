a8533917e4c44e647edc8624ebb2fc9400de837c
godlikeJia <godlikejia@126.com>
2016-06-24 16:29:13 +0800

    modify ios push lib

--- xchat/xpush/push/push.go
+++ xchat/xpush/push/push.go
@@ -3,16 +3,17 @@ package push
 import (
 	"errors"
 	"fmt"
-	"xim/xchat/xpush/xinge"
+	xg "xim/xchat/xpush/xg"
 	"strings"
 	"time"
 	"xim/xchat/xpush/userinfo"
+	"xim/xchat/xpush/xinge"
 )
 
 const (
 	ACCESS_ID_ANDROID  = 2100108857                         //正式版安卓的accessId
 	SECRET_KEY_ANDROID = "bf5b940421c7edbf620622c4d7255a12" //正式版安卓的secretKey
-	ACCESS_ID_IOS      = 2200108858                         //正式版ios的accseeId
+	ACCESS_ID_IOS      = "2200108858"                       //正式版ios的accseeId
 	SECRET_KEY_IOS     = "87745f87793d4070fa52cfbabb0baa61" //正式版ios的secretKey
 
 	ACCESS_ID_IOS_ENT  = 2200141892
@@ -21,7 +22,7 @@ const (
 	//测试版属性
 	ACCESS_ID_ANDROID_TEST  = 2100118679                         //测试版安卓的accessId
 	SECRET_KEY_ANDROID_TEST = "d4bd574e15cfed0cf55b629a72072ce2" //测试版安卓的secretKey
-	ACCESS_ID_IOS_TEST      = 2200118680                         //测试版ios的accessId
+	ACCESS_ID_IOS_TEST      = "2200118680"                       //测试版ios的accessId
 	SECRET_KEY_IOS_TEST     = "6332c9644c963fb5d805103827f73fdf" //测试版ios的secretKey
 
 	ANDROID_ACTIVITY = "com.engdd.familytree" // android activity 用于唤醒Android
@@ -29,20 +30,21 @@ const (
 
 var (
 	androidClient *xinge.Client
-	iosClient     *xinge.Client
+	iosClient     *xg.Client
 )
 
 func NewPushClient(testing bool) {
 	if testing {
 		androidClient = xinge.NewClient(ACCESS_ID_ANDROID_TEST, SECRET_KEY_ANDROID_TEST)
-		iosClient = xinge.NewClient(ACCESS_ID_IOS_TEST, SECRET_KEY_IOS_TEST)
+		iosClient = xg.NewClient(ACCESS_ID_IOS_TEST, 300, "", SECRET_KEY_IOS_TEST)
 	} else {
 		androidClient = xinge.NewClient(ACCESS_ID_ANDROID, SECRET_KEY_ANDROID)
-		iosClient = xinge.NewClient(ACCESS_ID_IOS, SECRET_KEY_IOS)
+		//iosClient = xinge.NewClient(ACCESS_ID_IOS, SECRET_KEY_IOS)
+		iosClient = xg.NewClient(ACCESS_ID_IOS, 300, "", SECRET_KEY_IOS)
 	}
 }
 
-func PushOfflineMsg(from, to, source, token string, chatId, interval int64) error {
+func PushOfflineMsg(from, to, source, token string, chatId, interval int64, env byte) error {
 	l.Info("PushOfflineMsg %s", to)
 	ts, ok := userinfo.CheckLastPushTime(to, interval)
 	if !ok {
@@ -68,21 +70,28 @@ func PushOfflineMsg(from, to, source, token string, chatId, interval int64) erro
 		msg.Custom = map[string]string{"chat_id": szChatId}
 
 		resp = androidClient.PushSingleDevice(xinge.Android, token, msg)
-	} else { //if strings.ToLower(dev) == "iphone" {
-		resp = iosClient.PushSingleIosDevice(token, userName+"发来一条消息", 1,
-			map[string]string{"chat_id": szChatId})
+		if resp.Code != 0 {
+			return errors.New(resp.Msg)
+		}
+		return nil
+	} else {
+		aps := &xg.ApsAttr{Alert: userName + "发来一条消息", Badge: 1}
+		message := &xg.IosMessage{aps, map[string]interface{}{"chat_id": szChatId}}
+		reqPush := &xg.ReqPush{
+			PushType:     xg.PushType_single_device,
+			TagsOp:       xg.TagsOp_AND,
+			DeviceToken:  token,
+			MessageType:  xg.MessageType_ios,
+			Message:      message,
+			ExpireTime:   0,
+			SendTime:     time.Now(),
+			MultiPkgType: xg.MultiPkg_ios,
+			PushEnv:      xg.PushEnv(env),
+			PlatformType: xg.Platform_ios,
+			LoopTimes:    2,
+			LoopInterval: 7,
+			Cli:          iosClient,
+		}
+		return reqPush.Push()
 	}
-
-	if resp.Code != 0 {
-		return errors.New(resp.Msg)
-	}
-
-	return nil
 }
-
-//
-//func PushGroupMessage() {
-//	client := xinge.NewClient(ACCESS_ID_ANDROID_TEST, SECRET_KEY_ANDROID_TEST)
-//	msg := xinge.DefaultMessage("测试群", "LiLei: "+"Hello There!")
-//	client.PushSingleDevice(xinge.Android, "412936f4d21e80d84a77a7c756bd03e2da2f1c2e", msg)
-//}
