16323c9bc5e20c37ae9e5c54f8b9e05bf2dd236e
webee.yw <webee.yw@gmail.com>
2016-07-28 16:22:13 +0800

    add decode ns user.

--- utils/nsutils/nsutils.go
+++ utils/nsutils/nsutils.go
@@ -1,9 +1,22 @@
 package nsutils
 
+import (
+	"strings"
+)
+
 // EncodeNSUser encode ns user.
 func EncodeNSUser(ns, u string) (user string) {
 	if ns == "" {
 		return u
 	}
 	return ns + ":" + u
+}
+
+// DecodeNSUser decode nsUser.
+func DecodeNSUser(user string) (string, string) {
+	parts := strings.SplitN(user, ":", 2)
+	if len(parts) < 2 {
+		return "", user
+	}
+	return parts[0], parts[1]
 }
