3ec81e402db3014f536e9338f7f572cb99ac8394
webee.yw <webee.yw@gmail.com>
2016-07-30 13:15:47 +0800

    fix.

--- xchat/broker/mid/uris.go
+++ xchat/broker/mid/uris.go
@@ -2,8 +2,6 @@ package mid
 
 // uris.
 const (
-	URITestToUpper        = "xchat.test.to_upper"
-	URITestAdd            = "xchat.test.add"
 	URIWAMPSessionOnJoin  = "wamp.session.on_join"
 	URIWAMPSessionOnLeave = "wamp.session.on_leave"
 
